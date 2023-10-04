c3ce9e54d91652565ddc9ca96152b6b6d9f3fc85
Gabriel <146346779+moya500@users.noreply.github.com>
2023-10-04 20:23:53 +0200

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="About">
+    <h1>About</h1>
+    <br>
+    <h2> Gabriel is a Brussels based student currently in his second year of Media & Information Design at LUCA School of Arts.
+With an imagination beyond his own comprehension, his personal works such as his music and his 3D projects often seem like something that stepped out of a carefully curated world filled with absurdity and lightheartedness. </h2>
+  </div>
+</template>
+
+<style>
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
---
+++ src/views/WorksView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import image from "../assets/images/complexityposter.png"
+import image1 from "../assets/images/kokenzonderwoorden.png"
+</script>
+
+
+<template>
+  <div class="Works">
+    <h1>Works</h1>
+  
+    <div>
+      <h3> 
+      Illustrations 
+      </h3>
+    </div>
+
+    <div>
+    <img :src="image">
+    </div>
+
+    <div>
+    <img :src="image1">
+    </div>
+
+  </div>
+    
+</template>
+
+<style>
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+
+img{
+  width: 600px;
+}
+</style>
+
